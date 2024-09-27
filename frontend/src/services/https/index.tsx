d9ab2a2f86a6dcfaaddd75e4db9da92fbc26c0b2
piyathida2003 <piyathida2004@hotmail.com>
2024-09-27 20:49:45 +0700

    แก้ frontend

--- frontend/src/services/https/index.tsx
+++ frontend/src/services/https/index.tsx
@@ -364,7 +364,8 @@ async function CreateConditionRefun( conditionRefunData : ConditionInterface) {
 
 async function submitRefundRequest(values: any, ticket: any){
   try {
-    const response = await fetch("/api/refund-request", {
+    // eslint-disable-next-line no-template-curly-in-string
+    const response = await fetch(`${apiUrl}/refund-request`, {
       method: "POST",
       headers: {
         "Content-Type": "application/json",
@@ -385,7 +386,6 @@ async function submitRefundRequest(values: any, ticket: any){
   }
 
 }
-
 
 export {
   GetMember,
